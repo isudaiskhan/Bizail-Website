be1bc399c6c5ae82c97de6b0b7b7e1dffc1d07bb
sudais khan <isudaiskhan54@gmail.com>
2024-07-06 15:28:18 +0500

    changes in footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
   return (
     <>
 
-           <footer className="bg-[#1b0a43] text-white py-7 mt-10">
+           <footer className="bg-gradient-to-r from-[#1b0a43] to-[#3f2182] text-white py-8 mt-10">
               <div className="max-w-[1200px] mx-auto flex flex-col md:flex-row items-center justify-center">
     
                 <div className="text-center md:text-left mb-2 md:mb-0">
